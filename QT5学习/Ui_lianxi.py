758cf495b48b9df27a4743e10a105fb64bc72c05
Livermore <xpfele@163.com>
2022-10-19 21:06:07 +0800

    处理表格

---
+++ QT5学习/Ui_lianxi.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'd:\pyfile\lianxi.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.widget = QtWidgets.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(30, 60, 321, 121))
+        self.widget.setObjectName("widget")
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.lineEdit = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 1, 0, 1, 3)
+        self.fileButton = QtWidgets.QPushButton(self.widget)
+        self.fileButton.setObjectName("fileButton")
+        self.gridLayout.addWidget(self.fileButton, 1, 3, 1, 1)
+        self.rpngButton = QtWidgets.QRadioButton(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.rpngButton.setFont(font)
+        self.rpngButton.setObjectName("rpngButton")
+        self.gridLayout.addWidget(self.rpngButton, 2, 0, 1, 2)
+        self.rjpgButton = QtWidgets.QRadioButton(self.widget)
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.rjpgButton.setFont(font)
+        self.rjpgButton.setObjectName("rjpgButton")
+        self.gridLayout.addWidget(self.rjpgButton, 2, 2, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.widget)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 3, 1, 1, 2)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "不理数据"))
+        self.label.setText(_translate("Form", "选择文件"))
+        self.fileButton.setText(_translate("Form", "选择文件"))
+        self.rpngButton.setText(_translate("Form", "单张表格"))
+        self.rjpgButton.setText(_translate("Form", "多张表格"))
+        self.pushButton.setText(_translate("Form", "开始修理"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
---
+++ QT5学习/处理识别原始表彰.py
@@ -0,0 +1,155 @@
+import re
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import *
+from Ui_lianxi import Ui_Form
+import sys
+import os 
+import pandas as pd
+#这是一个处理每天整理识别快递单后的表格工具
+class WinLabel(QMainWindow,Ui_Form):
+    def __init__(self):
+        super(WinLabel,self).__init__()
+        self.setupUi(self)
+        self.show()
+        self.filename = "c:/"
+        self.sheet= None
+        self.path = None
+
+    @pyqtSlot()
+    def on_fileButton_clicked(self):
+        #获取文件名
+        self.filename = QFileDialog.getOpenFileName(None,'选择文件',self.filename)
+        #输入框显示文件路径和文件名
+        self.lineEdit.setText(self.filename[0])
+    @pyqtSlot()
+    def on_pushButton_clicked(self):
+        #把文件字分割取目录路径
+        self.path = self.filename[0].rsplit('.',maxsplit = 1)[0]
+        if self.sheet ==1:
+            self.editsheetone()
+        elif self.sheet == 2:
+            self.editsheettow()
+  
+    @pyqtSlot()
+    def on_rjpgButton_clicked(self):
+        self.label.setText('现在处理多张表格')
+        self.sheet = 2
+    @pyqtSlot()
+    def on_rpngButton_clicked(self):
+        self.label.setText('现在处理单张表格')
+        self.sheet = 1
+
+    def editsheetone(self):
+        df = pd.read_excel(self.filename[0],header=None,names=['原数据','姓名','电话','地址','类目'])
+        df['原数据'] = df['原数据'].apply(lambda x:str(x).strip())
+        df['原数据'] = df['原数据'].apply(lambda x:str(x).replace('\n',''))
+        df['姓名'] = df['原数据'].apply(lambda x:str(x).split('1',1)[0]) 
+        df['电话'] = df['原数据'].str.extract(r'(\d{11})',expand = False)
+        df['地址'] = df['原数据'].str.extract(r'\d{11}(.*)',expand = False)
+        df['类目'] = df['地址'].apply(self.expand)
+        df['地址'] = df['地址'].apply(self.expandz)
+        df['类目'] = df['类目'].apply(self.expandy)
+        df.to_excel(self.path+"整理后的.xlsx",index=False)
+
+    def editsheettow(self):
+        df = pd.read_excel(self.filename[0],sheet_name=None,header=None,names=['原数据','姓名','电话','地址','类目'])
+        l = list(df.keys())
+        for i in l:
+            df[i]['原数据'] = df[i]['原数据'].apply(lambda x:str(x).strip())
+            df[i]['原数据'] = df[i]['原数据'].apply(lambda x:str(x).replace('\n',''))
+            df[i]['姓名'] = df[i]['原数据'].apply(lambda x:str(x).split('1',1)[0]) 
+            df[i]['电话'] = df[i]['原数据'].str.extract(r'(\d{11})',expand = False)
+            df[i]['地址'] = df[i]['原数据'].str.extract(r'\d{11}(.*)',expand = False)
+            df[i]['类目'] = df[i]['地址'].apply(self.expand)
+            df[i]['地址'] = df[i]['地址'].apply(self.expandz)
+            df[i]['类目'] = df[i]['类目'].apply(self.expandy)
+        with pd.ExcelWriter(self.path+"整理后.xlsx") as writer:
+            for i in l:
+                df[i].to_excel(writer,sheet_name = i,index=False)
+    
+    def expand(self,x):
+    #识别类目
+        if "一建" in x:
+            p = re.search(r'一建(.*)',x).group()
+        elif '二建' in x:
+            p = re.search(r'二建(.*)',x).group()
+        elif '二级消防' in x:
+            p = re.search(r'二级消防(.*)',x).group()
+        elif '消防' in x:
+            p = re.search(r'消防(.*)',x).group()
+        elif '护士' in x:
+            p = re.search(r'护士(.*)',x).group()
+        elif '初级' in x:
+            p = re.search(r'初级(.*)',x).group()
+        elif '中级会计' in x:
+            p = re.search(r'中级(.*)',x).group()
+        elif '中级经济师' in x:
+            p = re.search(r'中级(.*)',x).group()
+        elif '司法' in x:
+            p = re.search(r'司法(.*)',x).group()
+        elif '证券' in x:
+            p = re.search(r'证券(.*)',x).group()
+        elif '基金' in x:
+            p = re.search(r'基金(.*)',x).group()
+        elif '安全' in x:
+            p = re.search(r'中级(.*)',x).group()
+        elif "注册会计" in x:
+            p = re.search(r'CPA(.*)',x).group()
+        elif '造价' in x:
+            p = re.search(r'造价(.*)',x).group()
+        elif '四川' in x:
+            p = re.search(r'四川(.*)',x).group()
+        elif '广东' in x:
+            p = re.search(r'广东(.*)',x).group()
+        elif '护理学' in x:
+            p = re.search(r'护理学(.*)',x).group()
+        elif '护师' in x:
+            p = re.search(r'护师(.*)',x).group()
+            
+        else:
+            return "无法识别"
+        return p
+
+    def expandz(self,x):
+        #去除地址中的店名和类目名
+        if "竹义"in x:
+            p = re.sub(r'竹义(.*)',"",x)
+        elif '星辰' in x:
+            p = re.sub(r'星辰(.*)',"",x)       
+        elif '易恒' in x:
+            p = re.sub(r'易恒(.*)',"",x)        
+        elif '才俊' in x:
+            p = re.sub(r'才俊(.*)',"",x)       
+        elif '库达' in x:
+            p = re.sub(r'库达(.*)',"",x)
+        else:
+            return x
+        return p
+
+    def expandy(self,x):
+        #去除地址中的店名
+        if "竹义"in x:
+            p = re.sub(r'竹义图书',"",x)
+        elif '星辰' in x:
+            p = re.sub(r'星辰图书',"",x)    
+        elif '易恒' in x:
+            p = re.sub(r'易恒图书',"",x)    
+        elif '才俊' in x:
+            p = re.sub(r'才俊图书',"",x)        
+        elif '库达' in x:
+            p = re.sub(r'库达图书',"",x)
+        else:
+            return x
+        return p
+
+
+    
+        
+
+        
+    
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = WinLabel()
+    sys.exit(app.exec_())
